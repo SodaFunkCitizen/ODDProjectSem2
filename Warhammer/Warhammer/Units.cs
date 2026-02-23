8340a9040af0f1cdf01ca89b7d518151a8b0647f
SodaFunkCitizen <S00267967@atu.ie>
2026-02-23 11:15:45 +0000

    fixing 2.0

---
+++ Warhammer/Warhammer/Units.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel;
+
+public class Unit : INotifyPropertyChanged
+{
+    public string Name { get; set; }
+    public string BattlefieldRole { get; set; }
+    public string Keywords { get; set; }
+    public int Points { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+}
