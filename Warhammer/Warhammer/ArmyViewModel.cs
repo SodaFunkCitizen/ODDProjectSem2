f25e348698f5713276c25aaf42b37e47a78cf55b
SodaFunkCitizen <S00267967@atu.ie>
2026-02-23 11:04:10 +0000

    Starter

---
+++ Warhammer/Warhammer/ArmyViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows;
+using Warhammer;
+
+public class ArmyViewModel : INotifyPropertyChanged
+{
+    public ObservableCollection<Unit> Units { get; set; }
+
+    private Unit _selectedUnit;
+    public Unit SelectedUnit
+    {
+        get => _selectedUnit;
+        set
+        {
+            _selectedUnit = value;
+            OnPropertyChanged(nameof(SelectedUnit));
+        }
+    }
+
+    public int TotalPoints => Units.Sum(u => u.Points);
+
+    // Commands
+    public RelayCommand AddUnitCommand { get; }
+    public RelayCommand RemoveUnitCommand { get; }
+    public RelayCommand ValidateCommand { get; }
+    public RelayCommand ExportCommand { get; }
+
+    public ArmyViewModel()
+    {
+        Units = new ObservableCollection<Unit>();
+        Units.CollectionChanged += (s, e) => OnPropertyChanged(nameof(TotalPoints));
+
+        AddUnitCommand = new RelayCommand(_ => AddUnit());
+        RemoveUnitCommand = new RelayCommand(_ => RemoveUnit(), _ => SelectedUnit != null);
+        ValidateCommand = new RelayCommand(_ => ValidateArmy());
+        ExportCommand = new RelayCommand(_ => ExportArmy());
+
+        LoadSampleData();
+    }
+
+    private void AddUnit()
+    {
+        var newUnit = new Unit
+        {
+            Name = "Intercessor Squad",
+            BattlefieldRole = "Battleline",
+            Keywords = "Infantry, Tacticus, Core",
+            Points = 95,
+            Wargear = new List<string>() { "Bolt Rifles", "Frag Grenades", "Krak Grenades" },
+            Abilities = new List<string>() { "Oath of Moment", "Objective Secured" }
+        };
+
+        Units.Add(newUnit);
+        SelectedUnit = newUnit;
+        OnPropertyChanged(nameof(TotalPoints));
+    }
+
+    private void RemoveUnit()
+    {
+        if (SelectedUnit != null)
+        {
+            Units.Remove(SelectedUnit);
+            SelectedUnit = null;
+            OnPropertyChanged(nameof(TotalPoints));
+        }
+    }
+
+    private void ValidateArmy()
+    {
+        if (TotalPoints > 2000)
+        {
+            MessageBox.Show("Army exceeds 2000 points!", "Validation");
+        }
+        else
+        {
+            MessageBox.Show("Army is valid!", "Validation");
+        }
+    }
+
+    private void ExportArmy()
+    {
+        string export = "Army List:\n\n";
+
+        foreach (var unit in Units)
+        {
+            export += $"{unit.Name} - {unit.Points} pts\n";
+        }
+
+        export += $"\nTotal: {TotalPoints} pts";
+
+        MessageBox.Show(export, "Export Preview");
+    }
+
+    private void LoadSampleData()
+    {
+        Units.Add(new Unit
+        {
+            Name = "Captain in Gravis Armour",
+            BattlefieldRole = "Character",
+            Keywords = "Character, Gravis, Leader",
+            Points = 105,
+            Wargear = new List<string>() { "Master-crafted Bolt Rifle", "Power Sword" },
+            Abilities = new List<string>() { "Rites of Battle", "Iron Resolve" }
+        });
+
+        Units.Add(new Unit
+        {
+            Name = "Redemptor Dreadnought",
+            BattlefieldRole = "Vehicle",
+            Keywords = "Vehicle, Dreadnought",
+            Points = 210,
+            Wargear = new List<string>() { "Macro Plasma Incinerator", "Onslaught Gatling Cannon" },
+            Abilities = new List<string>() { "Duty Eternal" }
+        });
+
+        OnPropertyChanged(nameof(TotalPoints));
+    }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string name)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
--- Warhammer/Warhammer/MainWindow.xaml.cs
+++ Warhammer/Warhammer/MainWindow.xaml.cs
@@ -1,28 +1,55 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace Warhammer
+namespace ArmyBuilder
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new ArmyViewModel();
         }
     }
 }
+
+public class Unit : INotifyPropertyChanged
+{
+    private string _name;
+    private string _battlefieldRole;
+    private string _keywords;
+    private int _points;
+
+    public string Name
+    {
+        get => _name;
+        set { _name = value; OnPropertyChanged(nameof(Name)); }
+    }
+
+    public string BattlefieldRole
+    {
+        get => _battlefieldRole;
+        set { _battlefieldRole = value; OnPropertyChanged(nameof(BattlefieldRole)); }
+    }
+
+    public string Keywords
+    {
+        get => _keywords;
+        set { _keywords = value; OnPropertyChanged(nameof(Keywords)); }
+    }
+
+    public int Points
+    {
+        get => _points;
+        set { _points = value; OnPropertyChanged(nameof(Points)); }
+    }
+
+    public List<string> Wargear { get; set; } = new List<string>();
+    public List<string> Abilities { get; set; } = new List<string>();
+
+    public event PropertyChangedEventHandler PropertyChanged;
+    protected void OnPropertyChanged(string name)
+        => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+}
+
