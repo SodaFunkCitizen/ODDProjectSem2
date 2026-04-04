42107a5f97a4c6f1b330016ec431e113f3205907
SodaFunkCitizen <S00267967@atu.ie>
2026-04-04 16:15:39 +0100

    Update Unit Window
    Warning starting the Back end of the project alot of this next stuff will be very rough and not be able to run I reckon. Alot to do before the build will function again

--- Warhammer/WarhammerArmyBuilder/AddUnitWindow.xaml.cs
+++ Warhammer/WarhammerArmyBuilder/AddUnitWindow.xaml.cs
@@ -11,17 +11,39 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+/*
+So basically this is the rough draft of the back end of the website.
+So there is the rough things i need this to do which is to add units from template and 
+I'm writing these notes just to show that this is my though process and not AI slop
+I am sorry about that fuck up before. 
+Now i'm gonna start on the unit SQL back end 
 
+I won't deny alot of the next few builds are not really gonna work 
+*/
 namespace WarhammerArmyBuilder
 {
-    /// <summary>
-    /// Interaction logic for Window1.xaml
-    /// </summary>
-    public partial class Window1 : Window
+
+    public partial class AddUnitWindow : Window
     {
-        public Window1()
+
+        public AddUnitWindow()
         {
             InitializeComponent();
+        }
+
+        private void AddFromTemplate_Click()
+        {
+           
+        }
+
+        private void AddCustom_Click()
+        {
+
+        }
+
+        private void Close_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
         }
     }
 }
